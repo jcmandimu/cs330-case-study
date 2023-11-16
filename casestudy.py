b0cc7c24f3919a54b63abea9de94fbe9cfd8a59a
Julie <jcm146@duke.edu>
2023-11-15 23:51:23 -0500

    Created t1 structure-might be bugs to fix

--- casestudy.py
+++ casestudy.py
@@ -4,59 +4,42 @@ import json as js
 import time
 import heapq
 from functions import *
+import queue
 
 
-driversdata=pd.read_csv('drivers.csv', nrows=5)
-passengersdata=pd.read_csv('passengers.csv', nrows=5)
+driversdata=pd.read_csv('drivers.csv',nrows=5)
+passengersdata=pd.read_csv('passengers.csv',nrows=5)
 
-def createpassengerstuple(passengersdata):
-    listpassengers=[]
-    datetimes=passengersdata["Date/Time"]
 
-    for i in range(len(datetimes)):
-        wait=abs(findelapsedtime(datetimes[i]))
-        tuplepassenger=(wait,(passengersdata["Date/Time"][i],passengersdata["Source Lat"][i],passengersdata["Source Lon"][i],passengersdata["Dest Lat"][i],passengersdata["Dest Lon"][i]))
-        listpassengers.append(tuplepassenger)
+def t1(driversdata,passengersdata):
+    driverstuples=createdriverstuple(driversdata)
+    passengerstuples=createpassengerstuple(passengersdata)
+
+    #create maxheap for passengers (organized by wait time)
+    heapq._heapify_max(passengerstuples)
+
+    #initializing FIFO queue for drivers
+    dq=queue.Queue(maxsize=len(driverstuples))
+    for d in driverstuples:
+        #put each driver in the queue
+        dq.put(d)
     
-    return listpassengers
+    #Create match
+    for passenger in passengersdata:
+         #pop out the firt elem aka first driver
+        driver_to_match=dq.get()
+        #and put it back into the back of the queue
+        dq.put(driver_to_match)
 
-
-def createdriverstuple(driversdata):
-    listdrivers=[]
-    datetimes=driversdata["Date/Time"]
-
-    for i in range(len(datetimes)):
-        wait=abs(findelapsedtime(datetimes[i]))
-        tupledriver=(wait,(driversdata["Date/Time"][i],driversdata["Source Lat"][i],driversdata["Source Lon"][i]))
-        listdrivers.append(tupledriver)
+        #Find match
+        next_passenger=heapq.heappop(passengerstuples)
+        match=(next_passenger[1],driver_to_match[1])
+        print(next_passenger)    
+        print(driver_to_match)
+        print(match)
+    return match
     
-    return listdrivers
 
-
-
-def findelapsedtime(requesttime):
-    currenttime = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    curr = datetime.strptime(currenttime, "%m/%d/%Y %H:%M:%S")
-    req = datetime.strptime(requesttime, "%m/%d/%Y %H:%M:%S")
-    elapsedtime = req - curr
-
-    return elapsedtime.total_seconds()
-
-
-
-driverstuples=createdriverstuple(driversdata)
-passengerstuples=createpassengerstuple(passengersdata)
-
-# print(driverstuples)
-print(passengerstuples)
-
-#create minheap
-
-heapq.heapify(driverstuples)
-heapq._heapify_max(passengerstuples)
-
-# print(heapq.heappop(driverstuples))
-print(heapq.heappop(passengerstuples))
-
+t1(driversdata,passengersdata)
 
 
