a494c8dad97321cf06b6cb7b69896dbae77b7b73
EwunetEndalk <--unset>
2023-11-20 17:57:49 -0500

    merged n ready 2 commit

--- casestudy.py
+++ casestudy.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from collections import deque
+from collections import deque, defaultdict
 import pandas as pd
 import json as js
 import time
@@ -8,42 +8,76 @@ from functions import *
 import queue
 import math
 
+driversdata = pd.read_csv("drivers.csv")
+passengersdata = pd.read_csv("passengers.csv")
+edge_data = pd.read_csv("edges.csv")
 
-driversdata=pd.read_csv('drivers.csv')
-passengersdata=pd.read_csv('passengers.csv')
+driverstuples = createdriverstuple(driversdata)
+passengerstuples = createpassengerstuple(passengersdata)
 
-driverstuples=createdriverstuple(driversdata)
-passengerstuples=createpassengerstuple(passengersdata)
+global edgetuples
+edgetuples = getEdgeInfo(edge_data)
 
+n = open("node_data.json")
+node_data = js.load(n)
+
+global nodetuples
+nodetuples = getNodeInfo(node_data)
+
+
+# defining what our graph looks like
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
+
+    def addEdgeToAdjMatrix(self, u, v, w):
+        # where w = the list with avg times given date/time
+        self.graph[u][v] = w
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+        # Initialize minimum distance for next node
+        min = 1e7
+
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
 
 
 def t1():
-    #create maxheap for passengers (organized by wait time)
+    # create maxheap for passengers (organized by wait time)
     heapq._heapify_max(passengerstuples)
 
-    #initializing FIFO queue for drivers
-    dq=queue.Queue(maxsize=len(driverstuples))
+    # initializing FIFO queue for drivers
+    dq = queue.Queue(maxsize=len(driverstuples))
     for d in driverstuples:
-        #put each driver in the queue
+        # put each driver in the queue
         dq.put(d)
-        
-    #Create match
+
+    # Create match
     for passenger in passengerstuples:
-         #pop out the firt elem aka first driver
-        driver_to_match=dq.get()
-        #and put it back into the back of the queue
+        # pop out the firt elem aka first driver
+        driver_to_match = dq.get()
+        # and put it back into the back of the queue
         dq.put(driver_to_match)
 
-        #Find match
-        next_passenger=heapq._heappop_max(passengerstuples)
-        match=(next_passenger[1],driver_to_match[1])
-        # print(next_passenger)    
+        # Find match
+        next_passenger = heapq._heappop_max(passengerstuples)
+        match = (next_passenger[1], driver_to_match[1])
+        # print(next_passenger)
         # print(driver_to_match)
         print(match)
-    return 
-    
-# t1()
- 
+    return
+
+
 def t2():
     # dictionary with driver-passanger pairs as keys and distances as values
     dpDistances = defaultdict(list)
@@ -53,91 +87,182 @@ def t2():
 
     heapq._heapify_max(passengerstuples)
 
-    #initializing FIFO queue for drivers
-    dq=deque()
+    # initializing FIFO queue for drivers
+    dq = deque()
     for d in driverstuples:
-        #put each driver in the queue
+        # put each driver in the queue
         dq.append(d)
-    
-    #Create match
+
+    # Create match
     for passenger in passengerstuples:
-        
-        if(not passengerstuples):
+        if not passengerstuples:
             return
-        if(not dq):
+        if not dq:
             return
-        
-        next_passenger=heapq._heappop_max(passengerstuples)
-    
+
+        next_passenger = heapq._heappop_max(passengerstuples)
+
         driver_to_match = min(dpDistances.get(next_passenger), key=lambda x: x[1])
         driver_to_match = driver_to_match[0]
-    
-        #if this is the closet match
-        if(dq[0] == driver_to_match):
-            #pop out the firt elem aka first driver
+
+        # if this is the closet match
+        if dq[0] == driver_to_match:
+            # pop out the firt elem aka first driver
             dq.popleft()
-            #and put it back into the back of the queue
+            # and put it back into the back of the queue
             # dq.push(driver_to_match)
         else:
-        #Find new match
-            if(driver_to_match not in dq):
+            # Find new match
+            if driver_to_match not in dq:
                 driver_to_match = dq.popleft()
             else:
                 dq.remove(driver_to_match)
-        
-        # driver_to_match 
-        match=(driver_to_match, next_passenger)
 
+        # driver_to_match
+        match = (driver_to_match, next_passenger)
 
-        # print(next_passenger)    
+        # print(next_passenger)
         # print(driver_to_match)
         # print(match)
-    return 
-    
-# t2()
-
-def t3():
-    dpTravelTimes = defaultdict(list)
-
-    heapq._heapify_max(passengerstuples)
-
-    #initializing FIFO queue for drivers
-    dq=deque()
-    for d in driverstuples:
-        #put each driver in the queue
-        dq.append(d)
-    
-    #Create match
-    for passenger in passengerstuples:
-        
-        if(not passengerstuples):
-            return
-        if(not dq):
-            return
-        
-        next_passenger=heapq._heappop_max(passengerstuples)
-    
-        driver_to_match = min(dpTravelTimes.get(next_passenger), key=lambda x: x[1])
-        driver_to_match = driver_to_match[0]
-    
-        #if this is the closet match
-        if(dq[0] == driver_to_match):
-            #pop out the firt elem aka first driver
-            dq.popleft()
-            #and put it back into the back of the queue
-            # dq.push(driver_to_match)
-        else:
-        #Find new match
-            if(driver_to_match not in dq):
-                driver_to_match = dq.popleft()
-            else:
-                dq.remove(driver_to_match)
-        
-        # driver_to_match 
-        match=(driver_to_match, next_passenger)
+    return
 
 
-        # print(next_passenger)    
-        # print(driver_to_match)
-        # print(match)
-    return 
+def buildEdgeWeights(t):
+    w_list = []
+    wkday_avg_times = []
+    wkend_avg_times = []
+    for wkday_speed in t[3]:
+        wkday_avg_times.append(
+            t[2] / wkday_speed
+        )  # length of road/avg speed on that given hour
+
+    for wkend_speed in t[4]:
+        wkend_avg_times.append(
+            t[2] / wkend_speed
+        )  # length of road/avg speed on that given hour
+
+    w_list.append(wkday_avg_times)
+    w_list.append(wkend_avg_times)
+    return w_list
+
+
+def buildGraph(edgetuples):  # will have to run this several times
+    newset = set()
+    for t in edgetuples:
+        newset.add(t[0])  # 0th element of each tuple is one vertex of tht edge
+        newset.add(t[1])  # 1st element of each tuple is other vertex of tht edge
+
+    network = Graph(max(newset))
+
+    for t in edgetuples:
+        w_list = buildEdgeWeights(t)
+
+    network.addEdgeToAdjMatrix(t[0], t[1], w_list)
+    return network
+
+
+# returns s: starting vertex for t3()/Dijkstra's
+def findClosestNode(driver_coords, nodes_arr):
+    # Base case: if the list is empty, return None
+    if not nodes_arr:
+        return None
+
+    if len(nodes_arr) == 1:
+        return nodes_arr[0]
+
+    # Divide the list into two halves
+    mid = len(nodes_arr) // 2
+    left = nodes_arr[:mid]
+    right = nodes_arr[mid:]
+
+    # Recursively find the closest coordinate in the left and right halves
+    closest_left = findClosestNode(driver_coords, left)
+    closest_right = findClosestNode(driver_coords, right)
+
+    # If the closest coordinate is in the left half, return it
+    if not closest_right or math.dist(closest_left, driver_coords) < math.dist(
+        closest_right, driver_coords
+    ):
+        return closest_left
+
+    # If the closest coordinate is in the right half, return it
+    return closest_right
+
+
+def t3(nodetuples, edgetuples):
+    # we want all the vertices
+    network = buildGraph(edgetuples)
+
+    # get output of t2 (the match tuple? i'm assuming we're
+    # returning that from t2 but idk)
+    match = t2()
+
+    # we just need the chosen driver from t2()
+    driver_coords = (match[0][1][1], match[0][1][2])
+
+    # key = node's coordinate; value = node id
+    nodes_dict = dict()
+    nodes_arr = []
+    for node in nodetuples:
+        nodes_dict[(node[2], node[1])] = node[0]
+        nodes_arr.append((node[2], node[1]))
+
+    closest_vertex = findClosestNode(driver_coords, nodes_arr)
+    s = nodes_dict.get(closest_vertex)
+
+
+# def t3():
+#     dpTravelTimes = defaultdict(list)
+
+#     heapq._heapify_max(passengerstuples)
+
+#     # initializing FIFO queue for drivers
+#     dq = deque()
+#     for d in driverstuples:
+#         # put each driver in the queue
+#         dq.append(d)
+
+#     # Create match
+#     for passenger in passengerstuples:
+#         if not passengerstuples:
+#             return
+#         if not dq:
+#             return
+
+#         next_passenger = heapq._heappop_max(passengerstuples)
+
+#         driver_to_match = min(dpTravelTimes.get(next_passenger), key=lambda x: x[1])
+#         driver_to_match = driver_to_match[0]
+
+#         # if this is the closet match
+#         if dq[0] == driver_to_match:
+#             # pop out the firt elem aka first driver
+#             dq.popleft()
+#             # and put it back into the back of the queue
+#             # dq.push(driver_to_match)
+#         else:
+#             # Find new match
+#             if driver_to_match not in dq:
+#                 driver_to_match = dq.popleft()
+#             else:
+#                 dq.remove(driver_to_match)
+
+#         # driver_to_match
+#         match = (driver_to_match, next_passenger)
+
+#         # print(next_passenger)
+#         # print(driver_to_match)
+#         # print(match)
+#     return
+
+
+def main():
+    # p = (9, 3)
+    # d = [(1, 0), (9, 4), (4, 2)]
+    # print(closestDriver(p, d))
+    print(t2())
+    # buildGraph(edgetuples)
+
+
+if __name__ == "__main__":
+    main()
--- functions.py
+++ functions.py
@@ -7,34 +7,150 @@ import math
 from collections import defaultdict
 
 
+driversdata = pd.read_csv("drivers.csv")
+passengersdata = pd.read_csv("passengers.csv")
+edge_data = pd.read_csv("edges.csv")
 
-driversdata=pd.read_csv('drivers.csv')
-passengersdata=pd.read_csv('passengers.csv')
+# Opening JSON file
+n = open("node_data.json")
 
-#t1 functions
+# returns JSON object as a dictionary
+node_data = js.load(n)
+
+
+def getNodeInfo(node_data):
+    list_nodes = []
+    for n_id in node_data:
+        list_nodes.append((n_id, node_data[n_id]["lat"], node_data[n_id]["lon"]))
+    return list_nodes
+
+
+# reading from edges.csv
+def getEdgeInfo(edge_data):
+    list_edges = []
+    for i, row in edge_data.iterrows():
+        # making part of edge tuple where you just get start_id, end_id, and length
+        tuple_edge = (
+            row["start_id"].astype(int),  # start_id
+            row["end_id"].astype(int),  # end_id
+            row["length"],  # length
+            # so sorry abt how silly and long this is
+            (
+                row["weekday_0"],
+                row["weekday_1"],
+                row["weekday_2"],
+                row["weekday_3"],
+                row["weekday_4"],
+                row["weekday_5"],
+                row["weekday_6"],
+                row["weekday_7"],
+                row["weekday_8"],
+                row["weekday_9"],
+                row["weekday_10"],
+                row["weekday_11"],
+                row["weekday_12"],
+                row["weekday_13"],
+                row["weekday_14"],
+                row["weekday_15"],
+                row["weekday_16"],
+                row["weekday_17"],
+                row["weekday_18"],
+                row["weekday_19"],
+                row["weekday_20"],
+                row["weekday_21"],
+                row["weekday_22"],
+                row["weekday_23"],
+            ),
+            (
+                row["weekend_0"],
+                row["weekend_1"],
+                row["weekend_2"],
+                row["weekend_3"],
+                row["weekend_4"],
+                row["weekend_5"],
+                row["weekend_6"],
+                row["weekend_7"],
+                row["weekend_8"],
+                row["weekend_9"],
+                row["weekend_10"],
+                row["weekend_11"],
+                row["weekend_12"],
+                row["weekend_13"],
+                row["weekend_14"],
+                row["weekend_15"],
+                row["weekend_16"],
+                row["weekend_17"],
+                row["weekend_18"],
+                row["weekend_19"],
+                row["weekend_20"],
+                row["weekend_21"],
+                row["weekend_22"],
+                row["weekend_23"],
+            ),
+        )
+        list_edges.append(tuple_edge)
+    # print(list_edges)
+    return list_edges
+    # debugging for weekday info:
+    # print("tuple w/ JUST weekdays: ")
+    # print(tuple(row["weekday_0":"weekday_23"]))
+    # print(str(len(tuple(row["weekday_0":"weekday_23"]))))
+
+    # tuple_edge += tuple(row["weekday_0":"weekday_23"])
+
+    # debugging for weekday info:
+    # print("tuple w/ JUST weekends: ")
+    # print(tuple(row["weekend_0":"weekend_23"]))
+    # print(str(len(tuple(row["weekend_0":"weekend_23"]))))
+
+    # tuple_edge += tuple(row["weekend_0":"weekend_23"])
+    # debugging for weekday info:
+    # print("\n length of tuple: ")
+    # print(str(len(tuple_edge))) #shoud be first three elements + two nested tuples
+
+    # print("\n num of rows: ")
+    # print(str(len(tuple_edge)-2 + len(tuple_edge[3]) + len(tuple_edge[4]))) #s
+
+
+# t1 functions
 def createpassengerstuple(passengersdata):
-    listpassengers=[]
-    datetimes=passengersdata["Date/Time"]
+    listpassengers = []
+    datetimes = passengersdata["Date/Time"]
 
     for i in range(len(datetimes)):
-        wait=abs(findelapsedtime(datetimes[i]))
-        tuplepassenger=(wait,(passengersdata["Date/Time"][i],passengersdata["Source Lat"][i],passengersdata["Source Lon"][i],passengersdata["Dest Lat"][i],passengersdata["Dest Lon"][i]))
+        wait = abs(findelapsedtime(datetimes[i]))
+        tuplepassenger = (
+            wait,
+            (
+                passengersdata["Date/Time"][i],
+                passengersdata["Source Lat"][i],
+                passengersdata["Source Lon"][i],
+                passengersdata["Dest Lat"][i],
+                passengersdata["Dest Lon"][i],
+            ),
+        )
         listpassengers.append(tuplepassenger)
-    
+
     return listpassengers
 
 
 def createdriverstuple(driversdata):
-    listdrivers=[]
-    datetimes=driversdata["Date/Time"]
+    listdrivers = []
+    datetimes = driversdata["Date/Time"]
 
     for i in range(len(datetimes)):
-        wait=abs(findelapsedtime(datetimes[i]))
-        tupledriver=(wait,(driversdata["Date/Time"][i],driversdata["Source Lat"][i],driversdata["Source Lon"][i]))
+        wait = abs(findelapsedtime(datetimes[i]))
+        tupledriver = (
+            wait,
+            (
+                driversdata["Date/Time"][i],
+                driversdata["Source Lat"][i],
+                driversdata["Source Lon"][i],
+            ),
+        )
         listdrivers.append(tupledriver)
-    
-    return listdrivers
 
+    return listdrivers
 
 
 def findelapsedtime(requesttime):
@@ -46,29 +162,69 @@ def findelapsedtime(requesttime):
     return elapsedtime.total_seconds()
 
 
+driverstuples = createdriverstuple(driversdata)
+passengerstuples = createpassengerstuple(passengersdata)
 
-driverstuples=createdriverstuple(driversdata)
-passengerstuples=createpassengerstuple(passengersdata)
+# print("The driver tuples: ")
+# print(driverstuples)
+# print(passengerstuples)
 
-#t2 functions 
-def calculateDistance(x1,x2,y1,y2): 
-    return math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+
+# t2 functiobs
+def calculateDistance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+
+
+def calculatingdpDistances(dists):
+    for driver in driverstuples:
+        for passenger in passengerstuples:
+            distance = calculateDistance(
+                driver[1][2], passenger[1][2], driver[1][1], passenger[1][1]
+            )
+            dists.update({(driver, passenger): distance})
+
+
+# t2 functions
+def calculateDistance(x1, x2, y1, y2):
+    return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+
 
 def calculatingDistances(dists):
     for driver in driverstuples:
         for passenger in passengerstuples:
-            distance = calculateDistance(driver[1][1], passenger[1][1], driver[1][2], passenger[1][2])
-            dists[passenger].append((driver,distance))
+            distance = calculateDistance(
+                driver[1][1], passenger[1][1], driver[1][2], passenger[1][2]
+            )
+            dists[passenger].append((driver, distance))
 
-#t3 functions
+
+# t3 functions
 def calculatingTravelTimes(times):
     for driver in driverstuples:
         for passenger in passengerstuples:
-            travelTime = calculateDistance(driver[1][1], passenger[1][1], driver[1][2], passenger[1][2])
-            times[passenger].append((driver,travelTime))
+            travelTime = calculateDistance(
+                driver[1][1], passenger[1][1], driver[1][2], passenger[1][2]
+            )
+            times[passenger].append((driver, travelTime))
+
 
 def findStart():
     return
+
+
 def findEnd():
     return
 
+
+def main():
+    # Opening JSON file
+    n = open("node_data.json")
+
+    # returns JSON object as a dictionary
+    node_data = js.load(n)
+    getNodeInfo(node_data)
+    getEdgeInfo(edge_data)
+
+
+if __name__ == "__main__":
+    main()
